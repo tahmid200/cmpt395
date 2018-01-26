f945f20f9773e41e8214b1b61229b72856f50f0d
tahmid <tahmid808@gmail.com>
2018-01-26 10:58:22 -0700

    worked on the models.py and created some classes for databasing

--- caraway/ParentList/models.py
+++ caraway/ParentList/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Parents(models.Model):
+    firstName = models.CharField(max_length=250)
+    lastName = models.CharField(max_length = 250)
+    email = models.CharField(max_length=250)
+    #children = models.CharField(max_length=250)
+
+class Children(models.Model):
+    children = models.ForeignKey(Parents,on_delete=models.CASCADE)
+    names = models.CharField(max_length=250)
