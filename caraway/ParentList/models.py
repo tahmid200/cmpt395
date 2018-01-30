c594fd2e48e850950c96f1a12c7c7890f19e87f5
tahmid <tahmid808@gmail.com>
2018-01-30 14:05:48 -0700

    added phone,and address

--- caraway/ParentList/models.py
+++ caraway/ParentList/models.py
@@ -5,7 +5,8 @@ class Parents(models.Model):
     firstName = models.CharField(max_length=250)
     lastName = models.CharField(max_length = 250)
     email = models.CharField(max_length=250)
-    #children = models.CharField(max_length=250)
+    address = models.CharField(max_length=250)
+    phone = models.CharField(max_length=250)
 
 class Children(models.Model):
     children = models.ForeignKey(Parents,on_delete=models.CASCADE)
