59152229575095b5b19b9defd8bc6df3a04f69d4
tahmid <tahmid808@gmail.com>
2018-02-12 13:14:38 -0700

    testing to include both karajit and raahym branch

--- caraway/caraway/settings.py
+++ caraway/caraway/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     #'ParentList.apps.ParentListConfig',
+    'login',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
