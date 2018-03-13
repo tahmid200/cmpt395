30bdf84dc54c4e6e44e4cee0c3ec017003e1bfbf
Raahym Asif <asifr@mymacewan.ca>
2018-03-12 20:19:21 -0600

    Kinda Ready to Demo

--- realCaraway/caraway/login/models.py
+++ realCaraway/caraway/login/models.py
@@ -7,7 +7,18 @@ class CustomUserManager(UserManager):
 
 class CustomUser(AbstractUser):
     objects = CustomUserManager()
-
+    #children1 = models.CharField(max_length=255)
+    #children2 = models.CharField(max_length=255)
 class ClassCreation(models.Model):
     classroom = models.CharField(max_length=255)
+
+class ParentCreation(models.Model):
+    username = models.CharField(max_length=254, blank=True)
+    email = models.EmailField(blank=True, unique=True)
+    password1 = models.CharField(max_length=255, blank=True)
+    password2 = models.CharField(max_length=254, blank=True)
+    children1 = models.CharField(max_length=255, blank=True)
+    children2 = models.CharField(max_length=255, blank=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
 
--- realCaraway/caraway/login/views.py
+++ realCaraway/caraway/login/views.py
@@ -1,23 +1,62 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
-from .models import ClassCreation
-from .forms import AdminCreationForm, ClassCreationForm,ParentCreationForm
+from .models import ClassCreation, ParentCreation
+from .forms import AdminCreationForm, ClassCreationForm, ParentCreationForm
 
-#parent 
-class SignUp(generic.CreateView):
-    form_class = ParentCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+
+#def SignUp(request):
+ #   if request.method == 'POST':
+  #      f = ParentCreationForm(request.POST)
+   #     if f.is_valid():
+    #        f.save()
+     #       messages.success(request, 'Account Created!')
+      #      return redirect('home')
+    #else:
+     #   f = ParentCreationForm()
+
+    #return render(render(request, 'signup.html' , {'form': f}))
+
+
+
+
+#parent
+
+
+#class SignUp(generic.CreateView):
+ #   form_class = ParentCreationForm
+  #  success_url = reverse_lazy('login')
+   # template_name = 'signup.html'
+
+
+def SignUp(request):
+    if request.method == 'POST':
+        form = ParentCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = request.POST.get('username' , '')
+            usernameObj = ParentCreation(username = username)
+            usernameObj.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('signup')
+
+    else:
+        form = ParentCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
+
+
 
 #admin
 class SignUpAdmin(generic.CreateView):
     form_class = AdminCreationForm
     success_url = reverse_lazy('admin')
     template_name = 'signupAdmin.html'
-
+#class
 def SignUpClass(request):
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
